7678954ff63afc36799bec3437b1fefeaf5bca5c
Wade Zimmerman <codingsurplus@gmail.com>
2024-08-31 15:59:58 -0500

    init

---
+++ .vitepress/config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Dev Map",
+  description: "A map of developer experiences"
+})
